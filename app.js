77f69010853a9832094b44456ee015e8e8803e72
Alberto Lanfranco <allan@ecco.com>
2025-12-13 14:24:40 +0100

    v3.1.9: Lock book card font sizes in landscape mode to prevent scaling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // App version (semantic versioning)
-const APP_VERSION = '3.1.8';
+const APP_VERSION = '3.1.9';
 console.log('Book Tracker app.js loaded, version:', APP_VERSION);
 
 // Helper functions for rating tags
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '3.1.8';
+const CACHE_VERSION = '3.1.9';
 const CACHE_NAME = `book-tracker-v${CACHE_VERSION}`;
 const urlsToCache = [
   '/',

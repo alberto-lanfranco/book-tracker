b1abe6f89eee84bfa0c34089ecba968af0752475
Alberto Lanfranco <allan@ecco.com>
2025-12-15 10:55:56 +0100

    fix: move datalist icon to input background for guaranteed visibility (v3.2.3)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // App version (semantic versioning)
-const APP_VERSION = '3.2.2';
+const APP_VERSION = '3.2.3';
 console.log('Book Tracker app.js loaded, version:', APP_VERSION);
 
 // Helper functions for rating tags
